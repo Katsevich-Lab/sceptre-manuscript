b1ed7c059e752b417cb858f1f72da872eb8955c1
xuranw <29867659+xuranw@users.noreply.github.com>
2021-04-19 00:14:00 -0400

    update Figure1a

--- sceptre_paper/plotting/Figure1.R
+++ sceptre_paper/plotting/Figure1.R
@@ -7,58 +7,52 @@ require(cowplot)
 source(paste0(code_dir, "/sceptre_paper/plotting/load_data_for_plotting.R"))
 fig1_dir <- paste0(manuscript_figure_dir, "/Figure1")
 
-# Subfigure a: Gasperini NTC p-values and Xie ARL15 p-value qqplots
-p_thresh <- 1e-21
-pa.1 <- original_results_gasp %>%
-  rename(pvalue = pvalue.raw) %>%
-  filter(site_type == "NTC", beta < 0) %>%
-  mutate(r = rank(pvalue), expected = ppoints(n())[r],
-         clower = qbeta(p=(1-ci)/2, shape1 = r, shape2 = n()+1-r),
-         cupper = qbeta(p=(1+ci)/2, shape1 = r, shape2 = n()+1-r)) %>%
-  filter(-log10(expected) > 2 | row_number() %% subsampling_factor == subsampling_factor) %>%
-  mutate(pvalue = ifelse(pvalue < p_thresh, p_thresh, pvalue)) %>%
-  ggplot(aes(x = expected, y = pvalue, ymin = clower, ymax = cupper)) +
-  geom_point(col = plot_colors[["gasperini_nb"]], size = 2, alpha = 0.5) +
-  geom_ribbon(alpha = 0.2) +
-  geom_abline(intercept = 0, slope = 1) +
-  xlab("") +
-  ylab("") +
-  ggtitle("Gasperini") +
-  scale_x_continuous(trans = revlog_trans(base = 10)) +
-  scale_y_continuous(trans = revlog_trans(base = 10)) +
-  theme_bw() + theme(
-                     legend.position = "none",
-                     panel.grid = element_blank(),
-                     panel.border = element_blank(),
-                     axis.line = element_line(),
-                     plot.title = element_text(hjust = 0.5))
+# Subfigure a: Gasperini NTC p-values and Xie negative control p-value qqplots
+p_thresh <- 1e-08
+temp_down = rbind(  # downsample Gasperini NTC p-values so that Xie and Gasperini have the same number of NTC pairs.
+  original_results_gasp %>%
+    rename(pvalue = pvalue.raw) %>%
+    filter(site_type == "NTC", beta < 0) %>% sample_n(85000) %>%
+    mutate(r = rank(pvalue), expected = ppoints(n())[r],
+           clower = qbeta(p=(1-ci)/2, shape1 = r, shape2 = n()+1-r),
+           cupper = qbeta(p=(1+ci)/2, shape1 = r, shape2 = n()+1-r)) %>%
+    filter(-log10(expected) > 0 | row_number() %% subsampling_factor == subsampling_factor) %>%
+    mutate(pvalue = ifelse(pvalue < p_thresh, p_thresh, pvalue)) %>%
+    select(pvalue, expected, clower, cupper) %>%
+    mutate(data = 'Gasperini'),
+  original_results_xie %>%
+    rename(pvalue = raw_p_val) %>%
+    filter(site_type == "negative_control") %>%
+    mutate(r = rank(pvalue), expected = ppoints(n())[r],
+           clower = qbeta(p=(1-ci)/2, shape1 = r, shape2 = n()+1-r),
+           cupper = qbeta(p=(1+ci)/2, shape1 = r, shape2 = n()+1-r)) %>%
+    filter(-log10(expected) > 0 | row_number() %% subsampling_factor == subsampling_factor) %>%
+    mutate(pvalue = ifelse(pvalue < p_thresh, p_thresh, pvalue)) %>% 
+    select(pvalue, expected, clower, cupper) %>%
+    mutate(data = 'Xie'))
 
-p_vals_hypergeo <- original_results_xie %>% pluck("arl15_enh")
-arl15_id <- filter(resampling_results_xie, gene_names == "ARL15") %>% pull(gene_id) %>% as.character()
-p_vals_hypergeo <- p_vals_hypergeo[names(p_vals_hypergeo) %in% resampling_results_xie$gene_id]
-p_vals_hypergeo <- p_vals_hypergeo[!(names(p_vals_hypergeo) %in% arl15_id)]
-
-pa.2 <- tibble(pvalue = p_vals_hypergeo, gene_id = names(p_vals_hypergeo)) %>%
-  mutate(r = rank(pvalue), expected = ppoints(n())[r],
-         clower = qbeta(p=(1-ci)/2, shape1 = r, shape2 = n()+1-r),
-         cupper = qbeta(p=(1+ci)/2, shape1 = r, shape2 = n()+1-r)) %>%
-  ggplot(aes(x = expected, y = pvalue, ymin = clower, ymax = cupper)) +
-  geom_point(col = plot_colors[["hypergeometric"]], size = 1, alpha = 0.5) +
-  geom_ribbon(alpha = 0.2) +
+p_a = ggplot(data = temp_down, aes(x = expected, y = pvalue, ymin = clower, ymax = cupper, color = data)) +
+  geom_point(size = 1.2, alpha = 0.5) + 
+  scale_color_manual(values = c(plot_colors[["gasperini_nb"]], plot_colors[["hypergeometric"]])) + 
+  geom_ribbon(alpha = 0.5, color = NA, fill = 'gray') +
   geom_abline(intercept = 0, slope = 1) +
   xlab("Expected null p-value") +
-  ylab("") +
-  ggtitle("Xie") +
+  ylab("Observed p-value") +
+  ggtitle("P-value miscalibration") +
   scale_x_continuous(trans = revlog_trans(base = 10)) +
   scale_y_continuous(trans = revlog_trans(base = 10)) +
   theme_bw() + theme(
-                     legend.position = "none",
-                     panel.grid = element_blank(),
-                     panel.border = element_blank(),
-                     axis.line = element_line(),
-                     plot.title = element_text(hjust = 0.5))
-top_left <- ggarrange(pa.1, pa.2, ncol = 1)
-pa_annotated <- annotate_figure(top_left, left = text_grob("Observed p-value gene-gRNA pair", rot = 90, vjust = 2.5))
+    #legend.position = "none",
+    legend.position = c(0.22, 0.77),
+    legend.title = element_blank(), 
+    legend.key = element_rect(colour = "transparent", fill = "transparent"),
+    legend.background = element_blank(),
+    legend.margin = margin(-0.25,0,0,0, unit="cm"),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    axis.line = element_line(),
+    plot.title = element_text(hjust = 0.5)) + 
+  guides(colour = guide_legend(override.aes = list(alpha = 1)))
 
 hjust <- -0.5
 vjust <- 3
@@ -140,7 +134,7 @@ p_e <- df %>% arrange(desc(KS_pval)) %>% mutate(KS_pval = ifelse(KS_pval < 1e-5,
                      legend.background = element_rect(fill = "transparent", colour = NA))
 
 # Combine everything through cowplot
-p_top <- plot_grid(pa_annotated, p_b, p_c, labels = c("a", "b", "c"), nrow = 1)
+p_top <- plot_grid(p_a, p_b, p_c, labels = c("a", "b", "c"), nrow = 1)
 p_bottom <- plot_grid(p_d, p_e, rel_widths = c(1,2), nrow = 1, labels = c("d", "e"))
 p_final <- plot_grid(p_top, p_bottom, nrow = 2, rel_heights = c(0.8, 1), align = "h")
 ggsave(filename = paste0(fig1_dir, "/figure1_abce.pdf"), plot = p_final, device = "pdf", scale = 1, width = 9.5, height = 7)
