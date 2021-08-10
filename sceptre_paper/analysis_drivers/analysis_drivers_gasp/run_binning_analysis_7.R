6d551f3546be1f028a787c4651a952cb71396f87
Tim <tbarry2@andrew.cmu.edu>
2021-08-10 13:50:55 -0400

    update fp of binning_analysis

--- sceptre_paper/analysis_drivers/analysis_drivers_gasp/run_binning_analysis_7.R
+++ sceptre_paper/analysis_drivers/analysis_drivers_gasp/run_binning_analysis_7.R
@@ -1,5 +1,5 @@
 args <- commandArgs(trailingOnly = TRUE)
-code_dir <- if (is.na(args[1])) "/Users/timbarry/research_code/sceptre-manuscript/" else args[1]
+code_dir <- if (is.na(args[1])) paste0(.get_config_path("LOCAL_CODE_DIR"), "sceptre-manuscript/") else args[1]
 # source args
 source(paste0(code_dir, "sceptre_paper/analysis_drivers/analysis_drivers_gasp/sceptre_function_args.R"))
 
