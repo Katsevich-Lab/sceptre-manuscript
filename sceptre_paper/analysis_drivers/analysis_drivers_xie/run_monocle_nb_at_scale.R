b13ddde900fabdda6cbb3b6286fd3cb614a31ee2
Tim <tbarry2@andrew.cmu.edu>
2021-09-08 20:28:04 -0400

    decrease running time of monocle analysis

--- sceptre_paper/analysis_drivers/analysis_drivers_xie/run_monocle_nb_at_scale.R
+++ sceptre_paper/analysis_drivers/analysis_drivers_xie/run_monocle_nb_at_scale.R
@@ -11,30 +11,30 @@ analysis_ready_dir <- paste0(offsite_dir, "data/xie/analysis_ready")
 cds <- readRDS(paste0(analysis_ready_dir, "/monocole_obj.rds"))
 pairs <- fst::read_fst(paste0(processed_dir, "/gRNA_gene_pairs.fst")) %>%
   dplyr::filter(type != "bulk_validation")
-if (small_example) pairs <- pairs[1:100,]
+gRNAs <- pairs$gRNA_id %>% as.character() %>% unique()
+if (small_example) gRNAs <- gRNAs[1:5]
 
 # set formula
 reduced_formula <- "~ log_n_umis + batch + log_n_gRNA_umis"
 
-# Loop over all gRNAs and genes; subset by gene, and regress on the corresponding gRNA using Molly's function
-# res <- future.apply::future_lapply(X = seq(1, nrow(pairs)), FUN = function(i) {
-res <- purrr::map_dfr(.x = seq(1, nrow(pairs)), .f = function(i) {
-  if (i %% 50 == 0) print (i)
-  gRNA <- as.character(pairs$gRNA_id[i])
-  gene <- as.character(pairs$gene_id[i])
-  # subset cds
-  cds_sub <- cds[gene,]
+res <- purrr::map_dfr(.x = gRNAs, function(gRNA) {
+  print(gRNA)
+  # filter pairs according to gRNA; get curr genes
+  curr_genes <- dplyr::filter(pairs, gRNA_id == !!gRNA) %>% dplyr::pull(gene_id) %>% as.character()
+  if (small_example) curr_genes <- curr_genes[1:5]
+  # subset cds according to curr_genes
+  cds_sub <- cds[curr_genes,]
   # set full formula
   full_formula <- paste0(reduced_formula, " + `", gRNA, "`")
-  # regress
+  # regress on all genes
   de <- myDifferentialGeneTest(cds = cds_sub,
-                                fullModelFormulaStr = full_formula,
-                                reducedModelFormulaStr = reduced_formula,
-                                relative_expr = TRUE,
-                                cores = 1,
-                                verbose = TRUE)
-  out <- data.frame(gRNA_id = gRNA, gene_id = gene, p_val = de$pval, q_val = de$qval, beta = de$beta, intercept = de$intercept)
-  return(out)
+                               fullModelFormulaStr = full_formula,
+                               reducedModelFormulaStr = reduced_formula,
+                               relative_expr = TRUE,
+                               cores = 1,
+                               verbose = TRUE)
+  out <- data.frame(gRNA_id = gRNA, gene_id = de$id, p_val = de$pval, q_val = de$qval, beta = de$beta, intercept = de$intercept)
+  return(out)  
 })
 
 saveRDS(res, paste0(results_dir_negative_binomial, "/monocle_nb_results.rds"))
